83f0bdd6f425829265889e8e5d394eb96dfb1271
Jason Chen <windson7@gmail.com>
2025-04-27 21:31:08 -0700

    practices on various queries

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,9 @@
+SELECT AVG(total_duration) AS average_total_duration
+FROM (
+  SELECT cohorts.name AS cohort, SUM(completed_at - started_at) as total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  GROUP BY cohort
+  ORDER BY total_duration
+) AS subquery;
